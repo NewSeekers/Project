a289d3a3d214b676b16caaf2b8c42ca1ba21f2cf
hs <hoys17@gmail.com>
2024-01-17 20:23:40 +0900

    feat: BDto, BoardController

--- 7. Web Programming/ProjectII/src/controller/BoardController.java
+++ 7. Web Programming/ProjectII/src/controller/BoardController.java
@@ -7,27 +7,49 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import boardCommand.BCommand;
+import boardCommand.BWriteCommand;
+
 /**
  * Servlet implementation class BoardController
  */
-@WebServlet("/BoardController")
+@WebServlet("/board/*.do")
 public class BoardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("board doGet 들어옴");
+		actionDo(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		System.out.println("board doPost 들어옴");
+		actionDo(request, response);
 	}
-
+	
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("board actionDo 들어옴");
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		String viewPage =null;
+		BCommand command =null;
+		
+		String uri = request.getRequestURI();
+		System.out.println("uri :" +uri);
+		String conPath = request.getContextPath();
+		System.out.println("conPath : "+conPath);
+		String servPath = request.getServletPath();
+		System.out.println("servPath : "+servPath);
+		String com = uri.substring(servPath.length());
+		
+		if(com.equals("/write_view.do")) {
+			viewPage = "write_view.jsp";
+		} else if(com.equals("/write.do")) {
+			command = new BWriteCommand();
+			command.execute(request, response);
+			
+		}
+	}
 }
