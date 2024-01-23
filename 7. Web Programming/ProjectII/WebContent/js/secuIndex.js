862c9360f8d27bfd423f084d977d3bb8fbcd1d54
SONG YANGMIN <greenkans@naver.com>
2024-01-24 08:34:50 +0900

    feat: delete no used secuindex.js

--- 7. Web Programming/ProjectII/WebContent/js/secuIndex.js
+++
@@ -1,8 +0,0 @@
-
-fetch('/exampleServlet')
-            .then(response => response.json())
-            .then(data => {
-                // 가져온 데이터를 화면에 출력
-                document.getElementById('data-container').innerText = data.message;
-            })
-            .catch(error => console.error('Error fetching data:', error));
